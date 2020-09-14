d32b0cded8849ca2635a995d511ed7bca56565bb
ckomodo <chrissakwa@gmail.com>
2020-09-14 00:27:13 -0700

    my first functiongit status

--- index.js
+++ index.js
@@ -1,64 +1,75 @@
-// console.log('Hello js');
+// // console.log('Hello js');
 
-// let name = 'sakwa' // string literal
-// console.log(name);
+// // let name = 'sakwa' // string literal
+// // console.log(name);
 
-// let firstName = 'js';
-// let lastName ='master';
-// console.log(firstName, lastName);
+// // let firstName = 'js';
+// // let lastName ='master';
+// // console.log(firstName, lastName);
 
-// const intRate = 3.3;
-// intRate = 3.3;
-// console.log(interestRate);
+// // const intRate = 3.3;
+// // intRate = 3.3;
+// // console.log(interestRate);
 
-// let age = 30; //number literal
+// // let age = 30; //number literal
 
-// let applicationDocs = true; //boolean literal
+// // let applicationDocs = true; //boolean literal
 
-// let firstName = undefined;
-// let lastName = null; 
+// // let firstName = undefined;
+// // let lastName = null; 
 
 
-// let name = 'sakwa';
-// let age = 30;
+// // let name = 'sakwa';
+// // let age = 30;
 
-let person ={
-name: "sakwa",
-age: 30
-};
+// let person ={
+// name: "sakwa",
+// age: 30
+// };
 
-// dot notation
-person.name = 'john';
+// // dot notation
+// person.name = 'john';
 
-// bracket notation
-person ['name'] = 'Mary'
+// // bracket notation
+// person ['name'] = 'Mary'
 
-console.log(person.name);
+// console.log(person.name);
 
-let selectedColors =['red', 'blue'];
-selectedColors[2] = 'green'
-console.log(selectedColors.length); //displays blue in the console
+// let selectedColors =['red', 'blue'];
+// selectedColors[2] = 'green'
+// console.log(selectedColors.length); //displays blue in the console
 
-//a function that performs a task - displays a value
-function greet(name, lastName){
-    console.log('hello ' + name + ' ' + lastName);
+// //a function that performs a task - displays a value
+// function greet(name, lastName){
+//     console.log('hello ' + name + ' ' + lastName);
+// }
+// greet('john', 'john');
+// greet('mary');
+
+// //function that calculates a value
+// function square (number){
+//     return number * number;
+// }
+// // let number = square(10);
+// console.log(square(5));
+
+// alert('this is it')
+// console.log(1);
+// let age = 4;
+// let year = 2020;
+// console.log(age, year);
+
+// age = 76;
+// console.log(age)
+
+// const cars = 6;
+// console.log(cars);
+
+// cars = 5;
+
+var msgOne = document.querySelector(".btnOne");
+function firstFunction(){
+    
+    msgOne = console.log("this is my first function");
 }
-greet('john', 'john');
-greet('mary');
-
-//function that calculates a value
-function square (number){
-    return number * number;
-}
-// let number = square(10);
-console.log(square(5));
-
-alert('this is it')
-console.log(1);
-let age = 4;
-let year = 2020;
-console.log(age, year);
-
-age = 76;
-console.log(age)
-
+document.addEventListener("click", firstFunction)
